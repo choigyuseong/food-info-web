e8d123cf5498bbe3e6462ba32f84b37c115ce423
choigyuseong <clitico12@gmail.com>
2025-06-13 22:34:48 +0900

    refactor: 검색 URI 경로 변경

--- routes/search.js
+++ routes/search.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router  = express.Router();
 const ctrl    = require('../controllers/searchController');
 
-router.get('/search', ctrl.searchPage);
+router.get('/', ctrl.searchPage);
 
 module.exports = router;
